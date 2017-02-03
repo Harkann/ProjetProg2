dc0e3c3932e86cfe46376bbcf125a3fb4e2938f3
Harkann <paulon@crans.org>
2017-02-03 18:55:00 +0100

    template de la classe pieces

---
+++ src/main/scala/pieces.scala
@@ -0,0 +1,33 @@
+abstract class Piece() {
+	val color
+	val position
+	def move
+	def take 
+	def is_legal
+
+	
+}
+
+class Queen() extends Piece {
+	
+}
+
+class Peon() extends Piece {
+	
+}
+
+class King() extends Piece {
+
+}
+
+class Tower() extends Piece {
+					
+}
+
+class Knight() extends Piece {
+	
+}
+
+class Bishop() extends Piece {
+	
+}
