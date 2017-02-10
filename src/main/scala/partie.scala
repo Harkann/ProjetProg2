1c1495461f58b7fdc116cef1f823ec0868f2b1df
fnareoh <garance.gourdel@laposte.net>
2017-02-10 18:55:16 +0100

    char pour la couleur debut de def des id et bases des pieces

--- src/main/scala/partie.scala
+++ src/main/scala/partie.scala
@@ -1,12 +1,18 @@
+import Array._
+
 class Partie() {
 	var matrix_pieces = ofDim[String](8,8);
 	// contient l'id des pieces à leur position. vaut 0 si pas de piece a la position.
-	val player = "W";
-	val check = false; //gros 
+	var player = 'W';
+	var check = false; //gros 
 	def is_check() = check;
 	def next_turn() = {
-		if player == "W" {player = "B"}
-		else {player = "W"}
+		if (player == 'W') {player = 'B'}
+		else {player = 'W'}
 	}
 
+}
+
+object Projet{
+	var partie= new Partie()
 }
