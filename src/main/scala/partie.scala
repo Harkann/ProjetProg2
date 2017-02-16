2ef4b76fe8db677ae4fcb308d0bdc43bcbdb83a1
fnareoh <garance.gourdel@laposte.net>
2017-02-16 13:09:39 +0100

    un certain nombre de modification sur les piece premier try deplacement horizontal vertical et digonaux, initialisation de l'echequier

--- src/main/scala/partie.scala
+++ src/main/scala/partie.scala
@@ -9,6 +9,42 @@ class Partie() {
 	def next_turn() = {
 		if (player == 'W') {player = 'B'}
 		else {player = 'W'}
+
+		//definition des pieces blanches
+		new Peon('W',(2,1))
+		new Peon('W',(2,2))
+		new Peon('W',(2,3))
+		new Peon('W',(2,4))
+		new Peon('W',(2,5))
+		new Peon('W',(2,6))
+		new Peon('W',(2,7))
+		new Peon('W',(2,8))
+		new Tower('W',(1,1))
+		new Tower('W',(1,8))
+		new Knight('W',(1,2))
+		new Knight('W',(1,7))
+		new Bishop('W',(1,3))
+		new Bishop('W',(1,6))
+		new Queen('W',(1,4))
+		new King('W',(1,5))
+
+		//definition des pieces noires
+		new Peon('B',(7,1))
+		new Peon('B',(7,2))
+		new Peon('B',(7,3))
+		new Peon('B',(7,4))
+		new Peon('B',(7,5))
+		new Peon('B',(7,6))
+		new Peon('B',(7,7))
+		new Peon('B',(7,8))
+		new Tower('B',(8,1))
+		new Tower('B',(8,8))
+		new Knight('B',(8,2))
+		new Knight('B',(8,7))
+		new Bishop('B',(8,3))
+		new Bishop('B',(8,6))
+		new Queen('B',(8,4))
+		new King('B',(8,5))
 	}
 
 }
--- src/main/scala/pieces.scala
+++ src/main/scala/pieces.scala
@@ -4,7 +4,7 @@
 
 //color de type char car la comparaison string char est fausse
 
-abstract class Piece(color:Char,position : (Int,Int)) {
+abstract class Piece(color:Char,var position : (Int,Int)) {
 	//val color:Char; // 'B' ou 'W'
 	val name:String; 
 	var is_alive:Boolean;
@@ -18,9 +18,55 @@ abstract class Piece(color:Char,position : (Int,Int)) {
 }
 
 
-trait Horizontal {
-	def dpct_horiz(position:(Int,Int)) : List[(Int,Int)]{}  //utiliser des traits pour factoriser le code.
+trait Horizontal_Vertical {  //utiliser des traits pour factoriser le code.
+	def dpct_horiz(position:(Int,Int)) : List[(Int,Int)]={
+	var (i,j) = position 
+	var res : List[ (Int,Int) ] = List()
+	for( new_j <- (j+1) to 8) { res=res:+(i,new_j)} //est ce qu'on gere le fait de surveiller s'il n'y a pas de pieces ici ?????????????????????????????????????????????????????????????????
+	for( new_j <- 1 to (j-1)) { res=res:+(i,new_j)}
+	return res
+	}
+
+	def dpct_verti(position:(Int,Int)) : List[(Int,Int)]={
+	var (i,j) = position 
+	var res : List[ (Int,Int) ] = List()
+	for( new_i <- (i+1) to 8) { res=res:+(new_i,j)} 
+	for( new_i <- 1 to (i-1)) { res=res:+(new_i,j)}
+	return res
+	}
 }
+
+trait Diagonal_ {  //utiliser des traits pour factoriser le code.
+	def dpct_diag_R(position:(Int,Int)) : List[(Int,Int)]={
+	var (i,j) = position 
+	var res : List[ (Int,Int) ] = List()
+	if (i>=j) {
+	for( dpclt <- 1 to 8-i) { res=res:+(i+dpclt,j+dpclt)}
+	for( dpclt <- 1 to j-1) { res=res:+(i-dpclt,j-dpclt)}
+	}
+	else {
+	for( dpclt <- 1 to 8-j) { res=res:+(dpclt+i,j+dpclt)}
+	for( dpclt<- 1 to (i-1)) { res=res:+(i+dpclt,j+dpclt)}
+	}
+	return res
+	}
+
+	def dpct_diag_L(position:(Int,Int)) : List[(Int,Int)]={
+	var (i,j) = position 
+	var res : List[ (Int,Int) ] = List()
+	if (i+j<=9) {
+	for( dpclt <- 1 to j-1) { res=res:+(i+dpclt,j-dpclt)}
+	for( dpclt <- 1 to i-1) { res=res:+(i-dpclt,j+dpclt)}
+	}
+	else {
+	for( dpclt <- 1 to 8-i) { res=res:+(dpclt+i,j-dpclt)}    //je me méfie de ma programation des deplacements....
+	for( dpclt<- 1 to (8-j)) { res=res:+(i-dpclt,j+dpclt)}
+	}
+	return res
+	}
+}
+
+
 
 trait Id_creation {
 	def id_create(color:Char,name:String) : Int = {
@@ -38,35 +84,51 @@ trait Id_creation {
 
 
 
-class Queen(color:Char,position:(Int,Int)) extends Piece(color,position) with Id_creation {
+//Je mets tout mes commentaires qui sont valables pour toute pièce sur la reine uniquement
+
+class Queen(color:Char,pos:(Int,Int)) extends Piece(color,pos) with Id_creation { //si jamais on remet "position" et pas un autre nom soit "pos" position est considéré constante
 	val name = "Qu"
 	var is_alive= true
 	val id=color+name+id_create(color,name)
+
+	var (i,j) = position
+	// est ce qu'on devrait pas faire atention à ce que la position ne soit pas dejà prise?
+	Projet.partie.matrix_pieces(i)(j)=id
+ 
 }
 
-class Peon(color:Char,position:(Int,Int)) extends Piece(color,position) with Id_creation{
+class Peon(color:Char,pos:(Int,Int)) extends Piece(color,pos) with Id_creation{
 	val name="Pe"
 	var is_alive=true
 	val id=color+name+id_create(color,name)
+
+	var (i,j) = position
+	Projet.partie.matrix_pieces(i)(j)=id 
 }
 
-class King(color:Char,position:(Int,Int)) extends Piece(color,position) with Id_creation{
+class King(color:Char,pos:(Int,Int)) extends Piece(color,pos) with Id_creation{
 	val name="Ki"
 	var is_alive=true
 	val id=color+name+id_create(color,name)
 	//def is_check; 
+
+	var (i,j) = position
+	Projet.partie.matrix_pieces(i)(j)=id
 }
 
-class Tower(color:Char,position:(Int,Int)) extends Piece(color,position) with Id_creation{
+class Tower(color:Char,pos:(Int,Int)) extends Piece(color,pos) with Id_creation{
 	val name="To"
 	var is_alive=true
 	val id=color+name+id_create(color,name)
 }
 
-class Knight(color:Char,position:(Int,Int)) extends Piece(color,position) with Id_creation{
+class Knight(color:Char,pos:(Int,Int)) extends Piece(color,pos) with Id_creation{
 	val name="Kn"
 	var is_alive=true
 	val id=color+name+id_create(color,name)
+
+	var (i,j) = position
+	Projet.partie.matrix_pieces(i)(j)=id
 }
 
 class Bishop(color:Char,position:(Int,Int)) extends Piece(color,position) with Id_creation{
@@ -74,4 +136,6 @@ class Bishop(color:Char,position:(Int,Int)) extends Piece(color,position) with I
 	var is_alive=true
 	val id=color+name+id_create(color,name)
 
+	var (i,j) = position
+	Projet.partie.matrix_pieces(i)(j)=id
 }
